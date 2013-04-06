a64b4f4503251addfefd7d313e135593f979d56f
jstaffans <johannes.staffans@gmail.com>
2013-04-06 21:31:42 +0300

    First commit

---
+++ src/main/java/fi/bitrite/ticakkatoe/App.java
@@ -0,0 +1,13 @@
+package fi.bitrite.ticakkatoe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
